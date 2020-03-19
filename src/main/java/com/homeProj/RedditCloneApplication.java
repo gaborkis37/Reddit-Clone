913b8bb0149da79155ee6ab1f64a618837ee6bc2
Gabor Kis <gabor_kis@epam.com>
2020-03-19 16:38:14 +0100

    done creating Entities of Link/Comment/Vote

--- src/main/java/com/homeProj/RedditCloneApplication.java
+++ src/main/java/com/homeProj/RedditCloneApplication.java
@@ -1,13 +1,28 @@
 package com.homeProj;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import com.homeProj.config.SpringitProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SpringitProperties.class)
 public class RedditCloneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedditCloneApplication.class, args);
 	}
 
+	
+	@Bean
+	@Profile("dev")
+	CommandLineRunner runner() {
+		return args -> {
+			System.out.println("This is from dev");
+		};
+	}
 }
