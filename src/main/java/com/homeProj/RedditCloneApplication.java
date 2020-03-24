da173d914576bf0623a905b888ec0940820a3792
Gabor Kis <gabor_kis@epam.com>
2020-03-24 18:25:23 +0100

    DatabaseLoader moved to separate package, main layout added

--- src/main/java/com/homeProj/RedditCloneApplication.java
+++ src/main/java/com/homeProj/RedditCloneApplication.java
@@ -1,16 +1,10 @@
 package com.homeProj;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 
 import com.homeProj.config.SpringitProperties;
-import com.homeProj.domain.Comment;
-import com.homeProj.domain.Link;
-import com.homeProj.repository.CommentRepository;
-import com.homeProj.repository.LinkRepository;
 
 @SpringBootApplication
 @EnableConfigurationProperties(SpringitProperties.class)
@@ -20,21 +14,4 @@ public class RedditCloneApplication {
 		SpringApplication.run(RedditCloneApplication.class, args);
 	}
 
-	
-	@Bean
-	CommandLineRunner runner(LinkRepository linkRepo, CommentRepository commentRepo) {
-		return args -> {
-			Link link = new Link("Getting Started with spring boot 2","https://google.com");
-			linkRepo.save(link);
-			
-			Comment comment =  new Comment();
-			comment.setBody("This is the body");
-			comment.setLink(link);
-			
-			commentRepo.save(comment);
-			link.addComment(comment);
-			
-	
-		};
-	}
 }
--- src/main/java/com/homeProj/controller/LinkController.java
+++ src/main/java/com/homeProj/controller/LinkController.java
@@ -1,54 +1,16 @@
 package com.homeProj.controller;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.homeProj.domain.Link;
-import com.homeProj.repository.LinkRepository;
-
-@RestController
-@RequestMapping("/links")
+@Controller
 public class LinkController {
 
-	private LinkRepository linkRepository;
-
-	@Autowired
-	public LinkController(LinkRepository linkRepository) {
-		this.linkRepository = linkRepository;
+	@GetMapping("/foo")
+	public String foo(Model model) {
+		model.addAttribute("pageTitle", "fooTitle");
+		return "foo";
 	}
 
-	@GetMapping("/")
-	public List<Link> list() {
-		return linkRepository.findAll();
-	}
-
-	@PostMapping("/create")
-	public Link create(@ModelAttribute Link link) {
-		return linkRepository.save(link);
-	}
-
-	@GetMapping("/{id}")
-	public Optional<Link> read(@PathVariable Long id) {
-		return linkRepository.findById(id);
-	}
-
-	@PutMapping("/{id}")
-	public Link update(@PathVariable Long id,@ModelAttribute Link link) {
-		return linkRepository.save(link);
-	}
-
-	@DeleteMapping("/{id}")
-	public void delete(@PathVariable Long id) {
-		linkRepository.deleteById(id);
-	}
 }
