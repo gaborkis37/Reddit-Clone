1b9e0823d38bd521e945f3007acf93a1e2fb1343
Gabor Kis <gabor_kis@epam.com>
2020-03-29 14:15:43 +0200

    basic security config + user and role class created

---
+++ src/main/java/com/homeProj/domain/Role.java
@@ -0,0 +1,35 @@
+package com.homeProj.domain;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Role {
+
+	 	@Id
+	    @GeneratedValue
+	    private Long id;
+
+	    @NonNull
+	    private String name;
+
+	    @ManyToMany( mappedBy = "roles")
+	    private Collection<User> users;
+	
+}
