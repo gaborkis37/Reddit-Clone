aded7cb6ff3a7d456ed4864fceccb34b1e3c61f2
Gabor Kis <gabor_kis@epam.com>
2020-03-29 18:32:23 +0200

    added thymeleaf spring security dialect

--- src/main/java/com/homeProj/WebsiteRating.java
+++ src/main/java/com/homeProj/WebsiteRating.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 import com.homeProj.config.SpringitProperties;
 
@@ -22,5 +22,10 @@ public class WebsiteRating {
 	@Bean
 	PrettyTime prettytime() {
 		return new PrettyTime(new Locale(""));
+	}
+
+	@Bean
+	public SpringSecurityDialect securityDialect() {
+		return new SpringSecurityDialect();
 	}
 }
