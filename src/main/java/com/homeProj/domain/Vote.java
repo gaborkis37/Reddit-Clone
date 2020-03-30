506e462fc0333e1371a06070c99cb42e86e38215
Gabor Kis <gabor_kis@epam.com>
2020-03-30 11:24:57 +0200

    created relationship between link and vote entities

--- src/main/java/com/homeProj/domain/Vote.java
+++ src/main/java/com/homeProj/domain/Vote.java
@@ -3,18 +3,31 @@ package com.homeProj.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
 @NoArgsConstructor
-public class Vote {
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class Vote extends Auditable {
 
-	@Id
-	@GeneratedValue
-	private Long id;
-	private int vote;
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
+    private short direction;
+
+    @NonNull
+    @ManyToOne
+    private Link link;
 
 }
